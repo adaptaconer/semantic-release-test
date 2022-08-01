0007d55da22828e621d7d07a715ced63b6908f8b
Coner Murphy <cmurphy@adaptavist.com>
2022-08-01 09:19:07 +0100

    feat(beta): some other feature

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,9 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
+
+  // some other beta feature added
+
   return (
     <div className={styles.container}>
       <Head>
